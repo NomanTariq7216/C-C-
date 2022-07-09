04f087c8615ec7c4fd80776001c29872041e99aa
Noman <nomans7216@gmail.com>
2022-07-09 17:16:51 +0500

    SECOND CODE

---
+++ Area of circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float r,Result,Pi;
+	cout<<"Enter the redius of circle = ";
+	cin>>r;
+	Pi=3.14;
+	Result=(r*r)*Pi;
+	cout<<"Area of circle is = "<<Result;
+	return 0;
+}
